0764e36e9cb88f5de90a4a23427f01ce49874bbd
Marc Weinberger <marc.weinberger@me.com>
2022-01-14 21:45:48 +0100

    Prepare for portfolio creation

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import styles from '../styles/utils.module.css';
+import { Layout } from '../components/layout';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1 className={styles.title}>
+        Welcome to my Portfolio
+      </h1>
+    </Layout>
+  );
+};
+
+export default Home;
