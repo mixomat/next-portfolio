3910e6d10e728b4582c74b6f6a2809c87cb1e757
Marc Weinberger <marc.weinberger@me.com>
2022-01-29 21:49:34 +0100

    Add about me component

---
+++ src/components/about.tsx
@@ -0,0 +1,10 @@
+export function About() {
+  return (
+    <div>
+      <p>Hi, I’m Marc. I’m a freelance Software Engineer and I’m passionate about designing and implementing cloud-native software systems.</p>
+      <p>With many years of experience, working on a huge breadth of projects from user-facing web apps, big data processing platforms, messaging backend systems, and more, I truly believe in agile software development methods and DevOps culture.</p>
+      <p>I support agile teams adopting that same mindset and take responsibility for the software architecture, requirement analysis, feature implementation, code quality coaching and infrastructure / operations tasks.</p>
+      <p>My current focus is on Kotlin / Java / Spring backend applications, leveraging Kubernetes and the cloud native ecosystem as runtime environment, but I’m also having extensive knowledge in frontend application development with Angular / React.</p>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next';
 import styles from '../styles/utils.module.css';
 import { Layout } from '../components/layout';
+import { About } from '../components/about';
 
-const Home: NextPage = () => {
+const Index: NextPage = () => {
   return (
     <Layout>
       <h1 className={styles.title}>Welcome to my Portfolio</h1>
+      <About />
     </Layout>
   );
 };
 
-export default Home;
+export default Index;
